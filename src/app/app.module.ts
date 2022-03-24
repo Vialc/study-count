3831c8ad234bd8eea888565362b4bfab9aa02411
Vitor <costec222@gmail.com>
2022-03-24 20:58:38 -0300

    Adicionando Interface do Usuário

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login/login.component';
+import { UserInterfaceComponent } from './user-interface/user-interface.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    UserInterfaceComponent
   ],
   imports: [
     BrowserModule,
